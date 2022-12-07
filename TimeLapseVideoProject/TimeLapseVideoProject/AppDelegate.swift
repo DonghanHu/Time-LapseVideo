0010f3786d8acf02add2f54b11da8538a64997f2
hudh0827@vt.edu <36307709+DonghanHu@users.noreply.github.com>
2022-12-06 21:32:18 -0500

    Created a simple menu

--- TimeLapseVideoProject/TimeLapseVideoProject/AppDelegate.swift
+++ TimeLapseVideoProject/TimeLapseVideoProject/AppDelegate.swift
@@ -12,9 +12,71 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 
-
+    // private var window: NSWindow!
+    
+    private var statusItem: NSStatusItem!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+//        window = NSWindow(
+//            contentRect: NSRect(x: 0, y: 0, width: 480, height: 270),
+//            styleMask: [.miniaturizable, .closable, .resizable, .titled],
+//            backing: .buffered, defer: false)
+//        window.center()
+//        window.title = "No Storyboard Window"
+//        window.makeKeyAndOrderFront(nil)
+        
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let button = statusItem.button {
+            // button.title = "T"
+            
+            button.image = NSImage(named: NSImage.quickLookTemplateName);
+//            button.image = NSImage(pasteboardPropertyList: "1.circle", ofType: NSPasteboard.PasteboardType(rawValue: "1"))
+            
+        }
+        
+        setupMenus()
+    
+    }
+    
+    func setupMenus() {
+        // 1
+        let menu = NSMenu()
+
+        // 2
+        let one = NSMenuItem(title: "One", action: #selector(didTapOne) , keyEquivalent: "1")
+        menu.addItem(one)
+
+        let two = NSMenuItem(title: "Two", action: #selector(didTapTwo) , keyEquivalent: "2")
+        menu.addItem(two)
+
+        let three = NSMenuItem(title: "Three", action: #selector(didTapThree) , keyEquivalent: "3")
+        menu.addItem(three)
+
+        menu.addItem(NSMenuItem.separator())
+
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+
+        // 3
+        statusItem.menu = menu
+    }
+    private func changeStatusBarButton(number: Int) {
+        if let button = statusItem.button {
+            button.image = NSImage(pasteboardPropertyList: "\(number).circle", ofType: NSPasteboard.PasteboardType(rawValue: number.description))
+        }
+    }
+    
+    @objc func didTapOne() {
+        changeStatusBarButton(number: 1)
+    }
+
+    @objc func didTapTwo() {
+        changeStatusBarButton(number: 2)
+    }
+
+    @objc func didTapThree() {
+        changeStatusBarButton(number: 3)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
